e3157231cf22ba00284ea5541be0f34ba1d59d7c
james McBride <james.m.mcbride15@gmail.com>
2021-02-18 16:33:36 -0500

    adding changes while following React setState tutorials

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state={lat: 40};
+
+        const options = {
+            enableHighAccuracy: true,
+            maximumAge: 5000,
+            timeout: 5000
+        };
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({lat: position.coords.latitude})
+            },
+            (err) => console.log(err),
+            options
+        );
+    }
+
+    render(){
+
+        return <div>Latitude: {this.state.lat} </div>
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"))
